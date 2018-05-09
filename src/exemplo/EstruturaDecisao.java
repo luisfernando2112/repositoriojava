7a214fac4e1d9b0229835d6e1dab634a943aedfd
51115152017.2 <51115152017.2@51DE194259.senacrjedu.local>
2018-05-08 21:56:00 -0300

    aula03

---
+++ src/exemplo/EstruturaDecisao.java
@@ -0,0 +1,29 @@
+package exemplo;
+
+public class EstruturaDecisao {
+	public static void main (String [] args)  {
+		/**
+		 * Estrutura de decisão
+		 * if()
+		 * if() e else
+		 * if(), else if()
+		 * if(), else if(), else
+	 **/	
+		
+		/*Segundo exemplo */
+		
+		String materia = "Java";
+		
+		if(materia == "PHP") {
+			System.out.println("Disciplina do 4° modulo");
+		} else if (materia == "Banco de dados") {
+			System.out.println("Disciplina do 3° modulo");
+		} else if (materia == "Java") {
+			System.out.println("Disciplina do 5° modulo");
+		} else  {
+			System.out.println("Disciplina não é dos modulos de programação");
+			
+			
+			}
+		}
+	}
